4309c537bb53ad2ffc23906df67f3dd5f33ca6a4
Web__Dev <murod.london@gmail.com>
2022-12-08 19:50:44 +0000

    Project-1 '' Quotes "

--- app.js
+++ app.js
@@ -1,118 +1,31 @@
-// const title = document.querySelector('#main-heading');
+const btn = document.querySelector('#new-quote');
+const quote = document.querySelector('.quote');
+const person = document.querySelector('.person');
 
-// title.style.color = 'orange';
+const quotes = [
+  {
+    quote: `"The best way to find yourself is to lose yourself in the servise of others."`,
+    person: `Mahatma Gandhi`,
+  },
+  {
+    quote: `" If you want to live a happy life, tie it to a goal, not to people ot things"`,
+    person: `Albert Einstein`,
+  },
+  {
+    quote: `" Your time is limited, so do not waste it living someone else's life."`,
+    person: `Stive Jobs`,
+  },
+  {
+    quote: `"It does not matter how sliwly you go al long as you do not stop."`,
+    person: `Confucius`,
+  },
+];
 
-// // List Items
-// const listItems = Array.from(
-//   document.querySelectorAll('.list-item'),
-// );
+btn.addEventListener('click', () => {
+  const random = Math.floor(
+    Math.random() * quotes.length,
+  );
 
-// listItems.forEach((item) => {
-//   item.style.fontSize = '1.8rem';
-//   item.style.color = 'tomato';
-// });
-
-// // Creating Element
-// const ul = document.querySelector('ul');
-// const li = document.createElement('li');
-
-// // Adding Element
-// ul.append(li);
-
-// // Modifying Element
-// li.innerText = 'X-Men';
-
-// li.classList.add('list-item');
-
-// console.log(li.classList.contains('list-item'));
-
-// li.remove();
-
-// Event Probagation
-// Has 3 Phases
-// 1.Event Capturing
-// 2.Target
-// 3.Event Bubbling
-
-// window.addEventListener(
-//   'click',
-//   function () {
-//     console.log('Window');
-//   },
-//   false,
-// );
-
-// document.addEventListener(
-//   'click',
-//   function () {
-//     console.log('Document');
-//   },
-//   false,
-// );
-
-// document.querySelector('.div2').addEventListener(
-//   'click',
-//   function () {
-//     // e.stopPropagation();
-//     console.log('div 2');
-//   },
-//   { once: true },
-// );
-
-// document.querySelector('.div1').addEventListener(
-//   'click',
-//   function () {
-//     console.log('div 1');
-//   },
-//   false,
-// );
-
-// document.querySelector('.button').addEventListener(
-//   'click',
-//   function (e) {
-//     e.preventDefault();
-//     console.log(
-//       (e.target.innerText = 'Changed to clicked'),
-//     );
-//   },
-//   false,
-// );
-
-// Event Delegation.
-// It allows users to append a SINGLE event listener to a parent element
-// that adds it to all of its present AND future descendants that match a selector.
-
-// document
-//   .querySelector('#football')
-//   .addEventListener('click', (e) => {
-//     console.log('Football is clicked');
-
-//     const target = e.target;
-
-//     if (target.matches('li')) {
-//       target.style.backgroundColor = '#ba0000';
-//     }
-//   });
-
-document
-  .querySelector('#sports')
-  .addEventListener('click', function (e) {
-    console.log(
-      e.target.getAttribute('id') + 'is clicked',
-    );
-
-    const target = e.target;
-
-    if (target.matches('li')) {
-      target.style.backgroundColor = 'orange';
-    }
-  });
-
-// Create Element
-const sports = document.querySelector('#sports');
-const newSport = document.createElement('li');
-
-newSport.innerText = 'rugby';
-newSport.setAttribute('id', 'rugby');
-
-sports.appendChild(newSport);
+  quote.innerText = quotes[random].quote;
+  person.innerText = quotes[random].person;
+});
